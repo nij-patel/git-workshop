35ff826f4e55dde99b7e92a34e4f33e7b80ed0f9
Joshua <joshuaraj523@gmail.com>
2024-02-29 21:13:56 -0500

    I added subtract

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,10 @@ public class Calculator {
     }
 
     // TODO: Create subtract method
+    static int subtract(int a, int b)
+    {
+        return a-b;
+    }
 
     // TODO: Create divide method
 
